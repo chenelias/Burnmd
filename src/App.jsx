e92fc68cec66762772103a6e4f3eccbbb137584f
EliasChen <elias9988110@gmail.com>
2022-10-20 16:51:39 +0000

    navbar active style

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import Editor from './components/Editor'
 import './index.css'
 import Nav from './components/Nav'
 import React, { useState, useEffect } from 'react'
-// window.addEventListener('beforeunload', function (e) {
-//     var confirmationMessage =
-//         'It looks like you have been editing something. ' + 'If you leave before saving, your changes will be lost.'
-//     ;(e || window.event).returnValue = confirmationMessage
-//     return confirmationMessage
-// })
+window.addEventListener('beforeunload', function (e) {
+    var confirmationMessage =
+        'It looks like you have been editing something. ' + 'If you leave before saving, your changes will be lost.'
+    ;(e || window.event).returnValue = confirmationMessage
+    return confirmationMessage
+})
 function App() {
     const [togglemdfontsize, settogglemdfontsize] = useState(30)
     const [togglecolormode, settogglecolormode] = useState(getMode)
